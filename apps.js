c40f95aaeb705975c60bab2694e374726dca6dff
Murilo Góes <murilogoes25@gmail.com>
2024-09-07 05:44:57 -0300

    Finalizando

--- apps.js
+++ apps.js
@@ -3,28 +3,32 @@
 function pesquisar(){
 
     let section = document.getElementById("resultados-pesquisa");
-    console.log(section);
+    let campoPesquisa = document.getElementById("search-input").value;
+    if (campoPesquisa == ""){
+        section.innerHTML = "<p> Planeta não encontrado! Deve estar em outra dimensão!</p>" 
+        return
+    }
+  
     
     let resultados = ""
     // Variável para acumular o HTML gerado
     let htmlContent = '';
     
     for(let dado of dados) {
+        if(dado.nome.includes(campoPesquisa)||dado.nome.includes(campoPesquisa)) {
         resultados += `
         <div class="item-resultado">
             <h2>
-                <a href="${dado.link}" target="_blank">${dado.nome}</a>
+                <a href="#" target="_blank">${dado.nome}</a>
             </h2>
             <p class="descricao-meta">${dado.descricao}</p>
-        <a href="${dado.link}" target="_blank"> Abra seu satélite  </a>
+        <a href="${dado.link}" target="_blank"> Manuscritos da Nasa  </a>
         </div>`;
-    }
     
+        }
     // Após o loop, adicionar o conteúdo HTML ao 'section'
-    section.innerHTML = resultados;
+    }
 
 
+section.innerHTML = resultados;
 }
-
-
-
